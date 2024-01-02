f066ff09bf969cb3d6ff4446acc5c7689e577f5f
WIDANET <willianlps99@outlook.com>
2024-01-02 19:38:06 -0300

    update

--- Models/Estacionamento.cs
+++ Models/Estacionamento.cs
@@ -7,6 +7,22 @@ namespace DesafioEstacionamento.Models
 {
     public class Estacionamento
     {
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        private List<string> veiculos = new List<string>();
+
+    public Estacionamento(decimal precoInicial, decimal precoPorHora)
+    {
+        this.precoInicial = precoInicial;
+        this.precoPorHora = precoPorHora;
+    }
+
+    public void AdicionarVeiculo()
+    {
         
+    }
+
+
+
     }
 }
