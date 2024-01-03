b501a74e88475ea1eb35f860f560270431e99d81
WIDANET <willianlps99@outlook.com>
2024-01-03 16:46:55 -0300

    finish

--- Models/Estacionamento.cs
+++ Models/Estacionamento.cs
@@ -17,48 +17,48 @@ namespace DesafioEstacionamento.Models
     {
         this.precoInicial = precoInicial;
         this.precoPorHora = precoPorHora;
-
-        
     }
 
     public void AdicionarVeiculo()
     {
         Console.WriteLine("Digite a placa do veículo para estacionar: ");
         veiculos.Add(Console.ReadLine());
-
     }
 
     public void RemoverVeiculo()
     {
         Console.WriteLine("Digite a placa do veículo para remover: ");
-
         string placa = Console.ReadLine();
 
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
         {
+            Console.WriteLine("------------------------------------------------------------------");
             Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado: ");
             int horas = Convert.ToInt32(Console.ReadLine());
             decimal valorTotal = precoInicial + (horas * precoPorHora);
-
+            Console.WriteLine("------------------------------------------------------------------");
             Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de R$ {valorTotal}");
             veiculos.Remove(placa);
-
         }
         else
         {
+            Console.WriteLine("-----------------------------------------------------------------------------------------");
             Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
         }
 
     }
+
 
     public void ListarVeiculos()
     {
        if (veiculos != null == veiculos.Any()) 
        {
         Console.WriteLine("Os veículos são:");
+        int count = 0;
         foreach(string Total in veiculos)
         {
-            Console.WriteLine(Total);
+            count++;
+            Console.WriteLine($"Veículo {count} - {Total}");
         }
        }
        else
